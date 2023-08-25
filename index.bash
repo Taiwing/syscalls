619cdfdc1dfcf256458d83280b9234585b46e2c5
Yoann FOREAU <yoyo.fofo47@gmail.com>
2023-08-25 06:47:04 +0200

    feat: home page description

--- index.bash
+++ index.bash
@@ -53,6 +53,43 @@ print_syscalls() {
     cat <<EOF
 		<h1>Syscalls</h1>
 		<p>Last update: $(date -R)</p>
+		<p>
+			This website contains a list of every system call for each
+			architecture supported by the Linux kernel. The data is extracted
+			directly from the last version of the kernel source code. The script
+			that does this is executed at regular intervals, therefore it should
+			always be up-to-date.
+		</p>
+		<p>
+			The syscall lists are always complete, meaning that there won't be
+			any missing syscall. However, some syscalls might not have any
+			parameter listed. Most of the time, this is because the syscall
+			is not implemented yet. The status will be 'todo' in the csv files.
+			In some rare cases, this can be because the script was not able to
+			find or parse the syscall declaration. The status will be set to
+			'missing' in the csv files. Right now, this is only the case for
+			syscalls directly implemented in assembly. This is not yet supported
+			by the parsing script but it might be in the future.
+		</p>
+		<p>
+			Other syscalls might be lacking some information about their
+			parameters. This is because the syscall declaration was not explicit
+			enough. For example, the <code>oldolduname</code> syscall for the
+			i386 architecture is declared as follows:
+			<code>int sys_olduname(struct oldold_utsname *);</code>
+			As you can see, the parameter is not named. This is not a problem
+			for the kernel, but it is for the parsing script. In this case, the
+			status will be set to 'anon' and only the type of the parameter will
+			be listed.
+		</p>
+		<p>
+			Most of the syscalls will show up as 'ok' in the csv files. This
+			means that the data was successfully extracted from the kernel
+			source code. It should be accurate, complete and up-to-date.
+			However, if you find any error, please feel free to report it on the
+			<a href="https://github.com/Taiwing/syscalls/issues">github</a>
+			repository.
+		</p>
 		<a href="csv/syscalls.tar.gz" download>Download all syscalls as CSV</a>
 		<ul>
 EOF
