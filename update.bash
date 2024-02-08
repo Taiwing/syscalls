37cd796778c3d9fe4f8162dd60992af46aa46f13
Yoann FOREAU <yoyo.fofo47@gmail.com>
2024-02-08 18:02:31 +0100

    feat: log only if there has been changes
    Use a .old and a .new file to check this before registering the log.

--- update.bash
+++ update.bash
@@ -1,9 +1,23 @@
 #!/usr/bin/env bash
 
-cd $(dirname $0)
-{
-	echo Updating syscalls on $(date)
-	make re
-	docker compose up -d --build --force-recreate
-} >> /var/log/syscalls-update.log 2>&1
+# This script is used to update the syscalls and restart the docker container
 
+# log file to keep track of the updates
+LOG_FILE=/var/log/syscalls-update.log
+
+# go to repo directory and update the syscalls
+cd $(dirname $0)
+make re >> ${LOG_FILE}.new 2>&1
+
+if diff ${LOG_FILE}.old ${LOG_FILE}.new; then
+	# if old and new logs are same, remove the new log
+	rm ${LOG_FILE}.new
+else
+	# if old and new logs are different, update the logs
+	echo Updating syscalls on $(date) >> ${LOG_FILE}
+	cat ${LOG_FILE}.new >> ${LOG_FILE}
+	mv ${LOG_FILE}.new ${LOG_FILE}.old
+fi
+
+# restart the docker container
+docker compose up -d --build --force-recreate
